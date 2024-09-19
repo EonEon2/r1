eb2e2bd6b860ee64f0d37cd70bef2dcba5b13529
Dongeon Lee <duqdjs123@naver.com>
2024-09-19 15:08:45 +0900

    useState add

--- src/components/Count1.tsx
+++ src/components/Count1.tsx
@@ -4,18 +4,20 @@ function Count1():ReactElement {
 
     console.log("Count1...............");
 
-    const [count, setCount] = useState<number>(0);
+    const [count, setCount] = useState({num:1});
 
+    // count : 현재값 , setCount: 변경되는값
     const handleClick = (): void => {
-        setCount(count + 1);
+
+        setCount({num: count.num + 1});
     }
 
     return (
-        <div>
-            <div>
-                {count}
+        <div className="border-2 w-full h-1/4 bg-amber-200 justify-center flex m-3 p-3">
+            <div className="text-7xl">
+                {count.num * 2}
             </div>
-            <button onClick={handleClick}>INC</button>
+            <button className="border-2 p-2 justify-center" onClick={handleClick}>INC</button>
         </div>
     );
 }
