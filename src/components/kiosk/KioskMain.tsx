c7a146e8e170421011d6a2bb2f9deb4b97ee86f0
Dongeon Lee <duqdjs123@naver.com>
2024-09-20 15:05:37 +0900

    kiosk cart add

--- src/components/kiosk/KioskMain.tsx
+++ src/components/kiosk/KioskMain.tsx
@@ -1,6 +1,7 @@
 import ProductList from "./ProductList.tsx";
 import CartDiv from "./CartDiv.tsx";
-import {IProduct} from "../../types/kiosk.ts";
+import {ICartItem, IProduct} from "../../types/kiosk.ts";
+import {useState} from "react";
 
 
 function KioskMain() {
@@ -13,10 +14,30 @@ function KioskMain() {
         {pid:5, pname:'M5', price:7000, img:'http://localhost:8081/food/M5.jpeg'},
     ]
 
+    const [ cartItems, setCartItems] = useState<ICartItem[]>([]) // useState가 여러종류의 데이터타입을 사용하므로 ICartItem을 데이터타입으로 사용한다.
+
+    //addToCart - 상태를 변경하는것이지 다른걸 변경하는것이 아니므로 void
+    const addToCart = (product: IProduct) :void => {
+
+        // 선택한 물건이 카트안에 있는지 확인해서 수량증가
+        const target: ICartItem|undefined = cartItems.find( item => item.product.pid === product.pid )
+
+        if(!target) {
+            setCartItems([...cartItems, {product:product, qty:1}])
+            return;
+        }
+        else{
+            target.qty += 1
+            setCartItems([...cartItems])
+        }
+
+    }
+
+
     return (
         <div className='w-full h-full p-3 flex'>
             <div className='w-2/3 bg-amber-400 m-2 p-2'>
-                <ProductList products={products}></ProductList>
+                <ProductList products={products} addToCart={addToCart}></ProductList>
             </div>
 
             <div className='w-1/3 bg-amber-950 m-2 p-2'>
--- src/components/kiosk/ProductList.tsx
+++ src/components/kiosk/ProductList.tsx
@@ -1,18 +1,18 @@
 import {IProduct} from "../../types/kiosk.ts";
+import {ReactElement} from "react";
 
 // 어떤애들을 받을건지 인터페이스로 뺀다 (props)
 interface ProductListProps {
     products: IProduct[];
+    addToCart: (product: IProduct) => void;
 }
 
-function ProductList({products}: ProductListProps): JSX.Element {
-
-    console.log(products);
+function ProductList({products, addToCart}: ProductListProps): ReactElement {
 
     const productLI = products.map((product: IProduct) => { // 람다식에 {} 이면 return
         const {pid, pname, price, img} = product; //
         return (
-            <li className='border-2 w-1/4' key={pid}>
+            <li className='border-2 w-1/4' key={pid} onClick={() => addToCart(product)}>
                 {img && <img src={img} alt={pname} />}
                 {pname} - {price}
             </li>
